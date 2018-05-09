afcaf31dbeae35d53b6b60bae5827145ad2e33da
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-05-09 00:08:24 -0400

    restructuring output

--- process.py
+++ process.py
@@ -9,9 +9,7 @@ import jinja2
 join = os.path.join
 
 DIR = os.path.abspath('.')
-DIR_IN = join(DIR, 'items')
-DIR_OUT = join(DIR, 'docs')
-DIR_OUTIMG = join(DIR_OUT, 'img')
+DIR_ITEMS = join(DIR, 'items')
 
 tmpl = jinja2.Environment(
     loader=jinja2.FileSystemLoader('./templates'),
@@ -29,17 +27,13 @@ def find_image(base):
 
 
 def clean_up():
-    files = glob.glob(join(DIR_OUT, '*.html'))
-    for fn in files:
-        os.remove(fn)
-
-    files = glob.glob(join(DIR_OUTIMG, '*'))
+    files = glob.glob(join(DIR_ITEMS, '*.html'))
     for fn in files:
         os.remove(fn)
 
 
 def generate_index():
-    files = glob.glob(join(DIR_IN, '*.yaml'))
+    files = glob.glob(join(DIR_ITEMS, '*.yaml'))
     files = list(reversed(sorted(files)))
 
     items = []
@@ -47,29 +41,26 @@ def generate_index():
         name, ext = os.path.splitext(fn)
 
         fn_yaml = fn
-        fn_img0 = find_image(name)
-        fn_img1 = join(DIR_OUTIMG, os.path.basename(fn_img0))
+        fn_img = find_image(name)
 
-        shutil.copy(fn_img0, fn_img1)
-
-        item = {'imgurl': join('img', os.path.basename(fn_img1))}
+        item = {'imgurl': join('items', os.path.basename(fn_img))}
         item.update(yaml.load(open(fn_yaml)))
 
         template = tmpl.get_template('./image.html')
         html = template.render(item=item)
 
-        fn_html = 'img_{}.html'.format(os.path.basename(name))
-        fn_html = join(DIR_OUT, fn_html)
+        fn_html = '{}.html'.format(os.path.basename(name))
+        fn_html = join(DIR_ITEMS, fn_html)
         with open(fn_html, 'w') as f:
             f.write(html)
 
-        item.update({'link': os.path.basename(fn_html)})
+        item.update({'link': join('items', os.path.basename(fn_html))})
         items.append(item)
 
     template = tmpl.get_template('./index.html')
     item = template.render(items=items)
 
-    with open(join(DIR_OUT, 'index.html'), 'w') as fn:
+    with open(join(DIR, 'index.html'), 'w') as fn:
         fn.write(item)
     
 
